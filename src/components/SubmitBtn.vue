e50ffcfd7893597b9043a5ae4cc1e587e9805928
EarlEcho <734952325@qq.com>
2018-03-29 20:35:15 +0800

    登录注册

---
+++ src/components/SubmitBtn.vue
@@ -0,0 +1,104 @@
+<template>
+
+    <!--等待提交组件-->
+    <div class="submit-btn-w">
+        <x-button type="primary" @click.native="beginSubmit" :loading="loading" :disabled="disable">{{buttonText}}
+        </x-button>
+    </div>
+</template>
+
+<script>
+    import {XButton, Toast} from 'vux'
+    import functions from '@/functions/common'
+
+    export default {
+        components: {
+            XButton,
+            Toast
+        },
+        name: 'functions-submit-btn',
+        data() {
+            return {
+                loading: false,
+                buttonText: '提交',
+                disable: false
+            }
+        },
+        props: ['submitUrl', 'submitMethod', 'submitData', 'submitHandler', 'submitFormRef', 'btnText', 'beforeSubmit'],
+        mounted: function () {
+            this.buttonText = this.btnText || this.buttonText;
+            this.loadingText = "正在" + this.buttonText;
+        },
+        methods: {
+            beginSubmit: function () {
+                /*if (this.beforeSubmit) {
+//                    this.beforeSubmit();
+                    if (this.beforeSubmit() != true) {
+                        return
+                    }
+                }
+
+
+                this.loading = true;
+                this.buttonText = "正在" + this.buttonText;
+                this.disable = true;
+                if (this.submitMethod == 'GET') {
+                    axios({
+                        method: 'get',
+                        url: this.submitUrl,
+                        data: this.submitData
+                    }).then((response) => {
+                        this.afterSubmit(response);
+
+                    }).catch((response) => {
+                        console.log(response)
+                    })
+
+
+                } else {
+                    axios({
+                        method: 'post',
+                        url: this.submitUrl,
+                        data: this.submitData
+                    }).then((response) => {
+                        this.afterSubmit(response);
+
+                    }).catch((response) => {
+                        console.log(response)
+                    })
+                }*/
+
+            },
+            afterSubmit: function (response) {
+                //恢复提交按钮UI
+                this.loading = false;
+                this.buttonText = this.btnText || '提交';
+                this.disable = false;
+                //多条form相关的错误信息，增加rules, 且显示一次后不再有效
+                var triggerCountFromServer = 0;
+                //处理返回结果
+                var result = response.data;
+                console.log(result);
+                if (result.code == 0) {
+                    this.submitHandler(result);
+                } else {
+                    functions.toast(result.msg, 'top')
+                    return;
+                }
+
+            }
+        }
+    }
+</script>
+<style lang="less">
+    .submit-btn-w {
+        button {
+            width: 100%;
+        }
+    }
+
+    .weui-btn_disabled.weui-btn_primary {
+        background-color: #4aaff5;
+    }
+
+</style>
--- src/views/singin.vue
+++ src/views/singin.vue
@@ -1,22 +1,88 @@
 <template>
-    <div class="sing-in-w">
-        <h1>注册</h1>
+    <div class="sing-up-w">
+        <p class="sing-up-tile">注册</p>
+        <group ref="signUpForm" :model="signUpForm" class="sign-up-form-w">
+            <x-input is-type="china-mobile" v-model="signUpForm.mobile" type="number"
+                     required placeholder="手机号"></x-input>
+            <x-input placeholder="密码" v-model="signUpForm.pwd" type="password"
+                     required></x-input>
+
+            <submit-btn submit-url="/" submit-method="POST" :before-submit="beforeSubmit"
+                        :submit-data="signUpForm"
+                        :submit-handler="submitSuccess" submit-form-ref="signUpForm" btn-text="注册"
+                        class="login-btn"></submit-btn>
+        </group>
+        <p class="sign-up-other-action">
+            <router-link to="/sing-up">
+                <span class="g-lf">立即登录</span>
+            </router-link>
+        </p>
     </div>
 </template>
 
 <script>
+    import {Group, XInput} from 'vux'
+    import SubmitBtn from '@/components/SubmitBtn'
+
     export default {
         name: '',
-        components: {},
+        components: {Group, XInput, SubmitBtn},
         props: [],
         data() {
             return {
-                msg: 'hello vue'
+                signUpForm: {
+                    mobile: '',
+                    pwd: ''
+                }
             }
         },
-        methods: {}
+        methods: {
+            beforeSubmit() {
+
+            },
+            submitSuccess() {
+
+            }
+        }
     }
 </script>
-<style scoped lang="less">
+<style lang="less">
+    .sing-up-w {
+        position: absolute;
+        width: 100%;
+        min-height: 100%;
+        background-color: #f5f5f5;
+        padding: 2.5rem 2rem;
+        box-sizing: border-box;
+        .sing-up-tile {
+            font-size: 3rem;
+            margin-bottom: 2rem;
+        }
+        .sign-up-form-w {
+            margin: 0 auto;
+            .vux-no-group-title {
+                margin-top: 0;
+                background-color: #f5f5f5;
+            }
+            .vux-x-input {
+                margin-bottom: 2.3rem;
+                background-color: white;
+                border-radius: 10px;
+            }
+            .weui-cell:before {
+                border-top: none;
+            }
+            .weui-cells:after {
+                border-bottom: none;
+            }
+        }
+        .sign-up-other-action {
+            margin-top: 1.3rem;
+            span {
+                font-size: 1.4rem;
 
+                color: #939393;
+            }
+        }
+    }
 </style>
