65a24cb3c58cf3c0159c5811d5844277dc95c1c4
EarlEcho <734952325@qq.com>
2018-05-01 23:51:19 +0800

    增加无信息提示

---
+++ src/components/NoComment.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="no-info-w">
+        <img src="static/image/noinfo.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        components: {},
+        props: [],
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+<style lang="less">
+    .no-info-w {
+        text-align: center;
+        img {
+            position: relative;
+            width: 100%;
+        }
+    }
+</style>
